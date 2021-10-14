ebad6c248ff68e92941a54e1f77164a533e0fa06
王逍遥 <wangxiaoyao77@gmail.com>
2021-10-14 16:13:33 +0800

    路由跳转问题:home页面

--- src/page/Home/Page1/index.jsx
+++ src/page/Home/Page1/index.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-const Page1 = () => {
+const Page1 = (props) => {
+  useEffect(() => {
+    console.log("取值", props);
+    console.log(
+      "针对Link来取值：target=_blank时无法获取state值 ",
+      props.location
+    );
+  }, []);
   return <div>page1</div>;
 };
 
