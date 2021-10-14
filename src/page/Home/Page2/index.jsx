874c31d40990e5f533de35bb2c381f56453e1e15
王逍遥 <wangxiaoyao77@gmail.com>
2021-10-14 16:46:56 +0800

    路由跳转带参数问题

--- src/page/Home/Page2/index.jsx
+++ src/page/Home/Page2/index.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-const Page2 = () => {
+const Page2 = (props) => {
+  useEffect(() => {
+    console.log("取值手法相同", props);
+  }, []);
+
   return <div>page2</div>;
 };
 
